4798faef0647514ca0e1cf8b215cf598213a9367
SohpieY <tianlinsy2026@student.cis.edu.hk>
2024-12-02 20:40:27 +0800

    Okay

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public void initializeCruiseShip() {
         cruiseShip = new CruiseShip("Sophie's Ship", 1800);
 
+        // Create Passengers
         Passenger passengerOne = new PremiumPassenger("Katie Fu", 1000);
         Passenger passengerTwo = new SeniorPassenger("Meagan Hsu", 1001, 200.0);
         Passenger passengerThree = new PremiumPassenger("Bridget Tang", 1002);
@@ -19,6 +20,7 @@ public class Main {
         Passenger passengerFive = new SeniorPassenger("Ms. Nahar", 1004, 10000.0);
         Passenger passengerSix = new SeniorPassenger("Sophie Yan", 1005, 1000.0);
 
+        // Add Passengers to Cruise Ship
         cruiseShip.addPassenger(passengerOne);
         cruiseShip.addPassenger(passengerTwo);
         cruiseShip.addPassenger(passengerThree);
@@ -26,10 +28,16 @@ public class Main {
         cruiseShip.addPassenger(passengerFive);
         cruiseShip.addPassenger(passengerSix);
 
+        ArrayList<Activities> activitiesOne = new ArrayList<>();
+        ArrayList<Activities> activitiesTwo = new ArrayList<>();
+        ArrayList<Activities> activitiesThree = new ArrayList<>();
+
+        // Create Destinations
         Destination destinationOne = new Destination("Paris", activitiesOne);
         Destination destinationTwo = new Destination("New York", activitiesTwo);
         Destination destinationThree = new Destination("Barcelona", activitiesThree);
 
+        // Create Activities
         visitAttraction activityOne = new visitAttraction("Eiffel Tower Visit",
                 "Enjoy panoramic views from the Eiffel Tower",
                 75.0, 8,
@@ -55,6 +63,12 @@ public class Main {
         destinationTwo.addActivity(activityTwo);
         destinationThree.addActivity(activityThree);
 
+        activityOne.signUp(passengerOne); // Katie Fu signs up for Eiffel Tower
+        activityTwo.signUp(passengerTwo); // Meagan Hsu signs up for Broadway Night
+        activityTwo.signUp(passengerThree); // Bridget Tang also signs up for Broadway Night
+        activityThree.signUp(passengerFour); // Mr. Ewing signs up for City Walking Tour
+
+        // Create and Add Cabins
         Cabin cabinOne = new Cabin("1A", passengerOne, true);
         Cabin cabinTwo = new Cabin("1B", false);
         Cabin cabinThree = new Cabin("2A", false);
@@ -66,41 +80,19 @@ public class Main {
 
     public void runScenarios() {
         System.out.println("=== Scenario 1: Print Cruise Itinerary ===");
-        printItinerary();
+        cruiseShip.printItinerary();
 
-        System.out.println("\n=== Scenario 2: Print Passenger List ===");
-        printPassengerList();
+        System.out.println("\n=== Scenario 2: Cruise Ship Information ===");
+        cruiseShip.printCruise();
 
-        System.out.println("\n=== Scenario 3: Print Individual Passenger Details ===");
-        printPassengerDetails(cruiseShip.getPassengers().get(0));
-
-        System.out.println("\n=== Scenario 4: Print Available Activities ===");
+        System.out.println("\n=== Scenario 3: Print Available Activities ===");
         printAvailableActivities();
 
-        System.out.println("\n=== Scenario 5: Print Cabin Allocations and Activities ===");
+        System.out.println("\n=== Scenario 4: Print Cabin Allocations and Activities ===");
         printCabinAllocation();
     }
 
-    private void printItinerary() {
-        cruiseShip.printItinerary();
-    }
 
-    private void printPassengerList() {
-        cruiseShip.printCruise();
-    }
-
-    private void printPassengerDetails(Passenger passenger) {
-        System.out.println("Passenger Name: " + passenger.getName());
-        System.out.println("Passenger Number: " + passenger.getPassengerNum());
-        if (passenger instanceof SeniorPassenger) {
-            System.out.println("Balance: $" + ((SeniorPassenger) passenger).getBalance());
-        }
-        System.out.println("Activities Signed Up For:");
-        for (Activities activity : passenger.getActivitiesSignedUpFor()) {
-            System.out.println("- " + activity.getName() + " at " + activity.getDestination().getDestination() +
-                    " - Price: $" + activity.getCost());
-        }
-    }
 
     private void printAvailableActivities() {
         System.out.println("Available Activities:");
